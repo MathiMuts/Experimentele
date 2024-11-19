e7638c8d1b37c6e08ebbbc384bb99df0ae6354d1
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2024-11-19 19:22:48 +0100

    Practicum 5

--- Practicum5/data-analyse.py
+++ Practicum5/data-analyse.py
@@ -1,8 +1,14 @@
+from pathlib import Path
 import numpy as np
-import time
+from fit_file import X_sq
 
-while True:
-    file = "data/penetration/Al-9.txt"
+folder_path = Path("penetration/data/")
+
+values = []
+for file_name in [file.name for file in folder_path.iterdir()]:
+    file = f"penetration/data/{file_name}"
+    thickness_value = float(file_name.split("-")[1].replace(".txt", "").replace(",", "."))
+
     data = np.loadtxt(file).T
     N = len(data)
 
@@ -10,7 +16,42 @@ while True:
     s = np.sqrt(np.sum((data-gem)**2))/np.sqrt(N-1)
     error = s/np.sqrt(N)
 
-    print(data)
-    print(gem, error)
-    print(error/gem)
-    time.sleep(1)
+    values.append(tuple([thickness_value, gem, error, error/gem]))
+
+# -----------
+
+param_names = ['A', 'B']
+initial_guess = [1, 1]
+
+def model(params, x):
+    A, B = params
+    return A + (B)*(x)
+
+thicknesses = np.array([value[0] for value in values])
+thicknesses_E = np.zeros(len(thicknesses))
+intensity = np.array([value[1] for value in values])
+intensity_E = np.array([value[2] for value in values])
+
+sorted_indices = np.argsort(thicknesses)
+
+thicknesses = thicknesses[sorted_indices]
+thicknesses_E = thicknesses_E[sorted_indices]
+intensity = intensity[sorted_indices]
+intensity_E = intensity_E[sorted_indices]
+
+# data = ((thicknesses[:9], thicknesses_E[:9]), (np.log(intensity[:9]), intensity_E[:9]/intensity[:9] ))
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=True, graf1_title='Titel', graf1_x_label='Label'
+#         )
+
+
+data = ((thicknesses[9:], thicknesses_E[9:]), (np.log(intensity[9:]), intensity_E[9:]/intensity[9:] ))
+
+X_sq(data, param_names, initial_guess, model,
+        root_attempts=None, datafile=None,
+        VERBOSE=False, LaTeX=False,
+        PLOT=True, graf1_title='Titel', graf1_x_label='Label'
+        )
