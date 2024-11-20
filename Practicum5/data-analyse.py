0fd3bfdb2d0a01f6bd0d526b5237acc1996c38d7
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2024-11-20 19:02:52 +0100

    Lineaire fit op beide energieen

--- Practicum5/data-analyse.py
+++ Practicum5/data-analyse.py
@@ -39,20 +39,55 @@ thicknesses_E = thicknesses_E[sorted_indices]
 intensity = intensity[sorted_indices]
 intensity_E = intensity_E[sorted_indices]
 
-cutoff = 6
-data = ((thicknesses[:cutoff], thicknesses_E[:cutoff]), (np.log(intensity[:cutoff]), intensity_E[:cutoff]/intensity[:cutoff] ))
+""" Hieronder een fit op de laag energetische straling"""
+# cutoff = 6
+# data = ((thicknesses[:cutoff], thicknesses_E[:cutoff]), (np.log(intensity[:cutoff]), intensity_E[:cutoff]/intensity[:cutoff] ))
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=False, graf1_title='Titel', graf1_x_label='Label'
+#         )
+
+""" Hieronder een fit op de hoog energetische straling"""
+# start = 9
+# data = ((thicknesses[start:], thicknesses_E[start:]), (np.log(intensity[start:]), intensity_E[start:]/intensity[start:] ))
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=True, graf1_title='Titel', graf1_x_label='Label'
+#         )
+
+""" Hieronder eerste deel min hoog energetische straling volgens model"""
+# A = 3.72791982
+# B = -0.08375010
+
+# intensity_corrected = []
+# for i in range(len(intensity)):
+#     intensity_corrected.append(intensity[i] - np.exp((A + B*thicknesses[i])))
+
+
+# data = ((thicknesses[:9], thicknesses_E[:9]), (np.log(intensity_corrected[:9]), intensity_E[:9]/intensity_corrected[:9] ))
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=True, graf1_title='Titel', graf1_x_label='thickness (mm)', graf1_y_label='ln(I)'
+#         )
+
+""" Hieronder tweede deel min laag energetische straling volgens model"""
+A = 3.71603917
+B = -1.96933049
+
+intensity_corrected = []
+for i in range(len(intensity)):
+    intensity_corrected.append(intensity[i] - np.exp((A + B*thicknesses[i])))
+
+data = ((thicknesses[6:], thicknesses_E[6:]), (np.log(intensity_corrected[6:]), intensity_E[6:]/intensity_corrected[6:] ))
 
 X_sq(data, param_names, initial_guess, model,
         root_attempts=None, datafile=None,
         VERBOSE=False, LaTeX=False,
-        PLOT=True, graf1_title='Titel', graf1_x_label='Label'
-        )
-
-start = 9
-data = ((thicknesses[start:], thicknesses_E[start:]), (np.log(intensity[start:]), intensity_E[start:]/intensity[start:] ))
-
-X_sq(data, param_names, initial_guess, model,
-        root_attempts=None, datafile=None,
-        VERBOSE=False, LaTeX=False,
-        PLOT=True, graf1_title='Titel', graf1_x_label='Label'
+        PLOT=True, graf1_title='Titel', graf1_x_label='thickness (mm)', graf1_y_label='ln(I)'
         )
