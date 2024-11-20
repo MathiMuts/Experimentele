fc233afa8a8314233746beb6ea491199c9b538ce
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2024-11-20 19:38:18 +0100

    Samen koken na chemie

--- Practicum5/data-analyse.py
+++ Practicum5/data-analyse.py
@@ -26,10 +26,10 @@ initial_guess = [1, 1]
 def model(params, x):
     A, B = params
     return A + (B)*(x)
-
+achtergrond = 3.1
 thicknesses = np.array([value[0] for value in values])
 thicknesses_E = np.zeros(len(thicknesses))
-intensity = np.array([value[1] for value in values])
+intensity = np.array([value[1] - achtergrond for value in values])
 intensity_E = np.array([value[2] for value in values])
 
 sorted_indices = np.argsort(thicknesses)
@@ -39,7 +39,16 @@ thicknesses_E = thicknesses_E[sorted_indices]
 intensity = intensity[sorted_indices]
 intensity_E = intensity_E[sorted_indices]
 
-""" Hieronder een fit op de laag energetische straling"""
+""" NOTE: Hieronder alle data"""
+# data = ((thicknesses, thicknesses_E), (np.log(intensity), intensity_E/intensity ))
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=True, graf1_title='Titel', graf1_x_label='Label'
+#         )
+
+""" NOTE: Hieronder een fit op de laag energetische straling"""
 # cutoff = 6
 # data = ((thicknesses[:cutoff], thicknesses_E[:cutoff]), (np.log(intensity[:cutoff]), intensity_E[:cutoff]/intensity[:cutoff] ))
 
@@ -49,26 +58,34 @@ intensity_E = intensity_E[sorted_indices]
 #         PLOT=False, graf1_title='Titel', graf1_x_label='Label'
 #         )
 
-""" Hieronder een fit op de hoog energetische straling"""
+""" NOTE: Hieronder een fit op de hoog energetische straling"""
 # start = 9
 # data = ((thicknesses[start:], thicknesses_E[start:]), (np.log(intensity[start:]), intensity_E[start:]/intensity[start:] ))
 
 # X_sq(data, param_names, initial_guess, model,
 #         root_attempts=None, datafile=None,
-#         VERBOSE=False, LaTeX=False,
+#         VERBOSE=False, LaTeX=True,
 #         PLOT=True, graf1_title='Titel', graf1_x_label='Label'
 #         )
 
-""" Hieronder eerste deel min hoog energetische straling volgens model"""
-# A = 3.72791982
-# B = -0.08375010
+""" NOTE: Hieronder eerste deel min hoog energetische straling volgens model"""
+# A = 3.66
+# B = -0.095
+# A_e = 0.03
+# B_e = 0.006
+
 
 # intensity_corrected = []
 # for i in range(len(intensity)):
 #     intensity_corrected.append(intensity[i] - np.exp((A + B*thicknesses[i])))
 
+# intensity_corrected_E = []
+# for i in range(len(intensity_corrected)):
+#     intensity_corrected_E.append(np.sqrt(intensity_E[i]**2 + (np.exp((A + B*thicknesses[i])))**2 * A_e**2 + (thicknesses[i]* np.exp((A + B*thicknesses[i])))**2 * B_e**2))
+# intensity_corrected_E = np.array(intensity_corrected_E)
 
-# data = ((thicknesses[:9], thicknesses_E[:9]), (np.log(intensity_corrected[:9]), intensity_E[:9]/intensity_corrected[:9] ))
+# data = ((thicknesses[:9], thicknesses_E[:9]), (np.log(intensity_corrected[:9]), intensity_corrected_E[:9]/intensity_corrected[:9] ))
+# # data = ((thicknesses[:9], thicknesses_E[:9]), (np.log(intensity_corrected[:9]), intensity_E[:9]/intensity_corrected[:9] ))
 
 # X_sq(data, param_names, initial_guess, model,
 #         root_attempts=None, datafile=None,
@@ -77,17 +94,17 @@ intensity_E = intensity_E[sorted_indices]
 #         )
 
 """ Hieronder tweede deel min laag energetische straling volgens model"""
-A = 3.71603917
-B = -1.96933049
+# A = 3.71603917
+# B = -1.96933049
 
-intensity_corrected = []
-for i in range(len(intensity)):
-    intensity_corrected.append(intensity[i] - np.exp((A + B*thicknesses[i])))
+# intensity_corrected = []
+# for i in range(len(intensity)):
+#     intensity_corrected.append(intensity[i] - np.exp((A + B*thicknesses[i])))
 
-data = ((thicknesses[6:], thicknesses_E[6:]), (np.log(intensity_corrected[6:]), intensity_E[6:]/intensity_corrected[6:] ))
+# data = ((thicknesses[6:], thicknesses_E[6:]), (np.log(intensity_corrected[6:]), intensity_E[6:]/intensity_corrected[6:] ))
 
-X_sq(data, param_names, initial_guess, model,
-        root_attempts=None, datafile=None,
-        VERBOSE=False, LaTeX=False,
-        PLOT=True, graf1_title='Titel', graf1_x_label='thickness (mm)', graf1_y_label='ln(I)'
-        )
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=False,
+#         PLOT=True, graf1_title='Titel', graf1_x_label='thickness (mm)', graf1_y_label='ln(I)'
+#         )
