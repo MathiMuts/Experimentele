675b387147b0041b60ae7b935de80925519760de
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2024-11-22 18:24:01 +0100

    data visualisatie

--- Practicum5/data-analyse.py
+++ Practicum5/data-analyse.py
@@ -41,17 +41,17 @@ intensity = intensity[sorted_indices]
 intensity_E = intensity_E[sorted_indices]
 
 """ NOTE: Hieronder alle data"""
-# x, x_E, y, y_E = thicknesses, thicknesses_E, intensity, intensity_E
-# plt.errorbar(x, y, yerr=y_E, fmt='o', color='black', label='Datapunten', capsize=4)
+x, x_E, y, y_E = thicknesses, thicknesses_E, intensity, intensity_E
+plt.errorbar(x, y, yerr=y_E, fmt='o', color='black', label='Datapunten', capsize=4)
 
-# # Add labels, title, and legend
-# plt.xlabel('dikte Al [mm]')
-# plt.ylabel('$I$ $[arb. eenh.]$')
-# plt.title(r'Plot van de $\gamma$-straling in functie van de dikte Al')
-# plt.legend()
-# plt.grid(True)
+# Add labels, title, and legend
+plt.xlabel('dikte Al [mm]')
+plt.ylabel('$I$ $[arb. eenh.]$')
+plt.title(r'Plot van de $\gamma$-straling in functie van de dikte Al')
+plt.legend()
+plt.grid(True)
 
-# plt.show()
+plt.show()
 
 """ NOTE: Hieronder een fit op de laag energetische straling"""
 # cutoff = 6
