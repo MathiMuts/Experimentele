f1a4af759110923b7cf2e1b668787462d4acaf86
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2024-11-25 11:08:08 +0100

    analyse upload

---
+++ Practicum6/data-analyse.py
@@ -0,0 +1,7 @@
+from fit_file import X_sq
+
+# X_sq(data, param_names, initial_guess, model,
+#         root_attempts=None, datafile=None,
+#         VERBOSE=False, LaTeX=True,
+#         PLOT=True, graf1_title='Fit voor hoog energetische straling', graf1_x_label='dikte Al [mm]', graf1_y_label='$ln(I) [arb. eenh.]$'
+#         )
