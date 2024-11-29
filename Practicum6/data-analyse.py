860f7da42f8c67d8bd637d7e66fd456b74a5bc9c
michailognyanov <michail@ognyanovi.net>
2024-11-29 11:16:48 +0100

    2pi fixen

--- Practicum6/data-analyse.py
+++ Practicum6/data-analyse.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 file=np.loadtxt("Practicum6/data.txt", delimiter=' ').T
 file2=np.loadtxt("Practicum6/data2.txt", delimiter=' ').T
 
-f=file[0]/(2*np.pi)
+f=file[0]*(2*np.pi)
 A=file[1]
 phi=file[2]*np.pi/180
 
-f2=file2[0]/(2*np.pi)
+f2=file2[0]*(2*np.pi)
 A2=file2[1]
 phi2=file2[2]*np.pi/180
 
 
-f_E = np.ones_like(f)*0.01/(2*np.pi)
+f_E = np.ones_like(f)*0.01*(2*np.pi)
 A_E = np.ones_like(f)*2.5
 phi_E = np.ones_like(f)*3*np.pi/180
 
-f_E2 = np.ones_like(f2)*0.01/(2*np.pi)
-A_E2 = np.ones_like(f2)*1.0
+f_E2 = np.ones_like(f2)*0.01*(2*np.pi)
+A_E2 = np.ones_like(f2)*1
 phi_E2 = np.ones_like(f2)*3*np.pi/180
 
 #data 1 amplitude plotten
@@ -38,19 +38,20 @@ if False:
 
 #data1 fitten amplitude    
 if False:
-    param_names = ['F', 'w', 'm', 'n']
-    initial_guess = [0.1, 0.1, 0.1, 0.1]
+    print(len(f_E))
+    print(len(A_E))
+    param_names = ['w','B','C']
+    initial_guess = [2,0.1,0.01]
 
     def model(params, x):
-        F, w, m, n = params
-        return F/( m*np.sqrt((w**2 - x**2)**2 + (x*(n/m))**2) )
-    
+        w,B,C = params
+        return C/(np.sqrt((x**2-w**2)**2+(x*B)**2))
     data = ((f, f_E), (A, A_E))
 
     X_sq(data, param_names, initial_guess, model,
         root_attempts=None, datafile=None,
         VERBOSE=False, LaTeX=True,
-        PLOT=True, graf1_title='Fit en data amplitude', graf1_x_label='f [Hz]', graf1_y_label='A [mm]'
+        PLOT=True, graf1_title='Fit en data amplitude', graf1_x_label=r'$\omega$ [Hz]', graf1_y_label='A [mm]'
         )
 #data1 fitten frequentie    
 if False:
@@ -83,14 +84,14 @@ if False:
 
     plt.show()
 # data 2 amplitude fit
-if False:
-     param_names = ['F', 'w', 'm', 'n']
-     initial_guess = [0.1, 0.1, 0.1, 0.1]
+if True:
+     param_names = ['w','B','C']
+     initial_guess = [10,0.1,1]
 
      def model(params, x):
-         F, w, m, n = params
-         return F/( m*np.sqrt((w**2 - x**2)**2 + (x*(n/m))**2) )
-     
+        w,B,C = params
+        return C/(np.sqrt((x**2-w**2)**2+(x*B)**2))
+
      data = ((f2, f_E2), (A2, A_E2))
 
      X_sq(data, param_names, initial_guess, model,
@@ -115,7 +116,7 @@ if False:
         PLOT=True, graf1_title=r'Fit voor $\phi$(f)', graf1_x_label='f [Hz]', graf1_y_label=r'$1/tan(\phi)$ [/]'
         )
 #frequentie fit 2 niet lineair
-if True:
+if False:
     param_names = ['w', 'C','S']
     initial_guess = [0.26, -0.07, 1]
 
@@ -132,7 +133,7 @@ if True:
         )
 
  # frequentie fit1 semi-lineair DEZE IS ASS
-if True:
+if False:
     param_names = ['w', 'C']
     initial_guess = [1.69, 0.006]
 
