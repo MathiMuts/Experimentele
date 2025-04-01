712b6fbc0ec1854b86eb783fa95aebc1a0bb6d28
michailognyanov <michail@ognyanovi.net>
2025-04-01 18:36:23 +0200

    data

--- Practicum 13-VRIJE PROEF/analyse.py
+++ Practicum 13-VRIJE PROEF/analyse.py
@@ -4,7 +4,9 @@ from scipy.interpolate import CubicSpline
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 import os
-data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\test_data_vrijeproef.csv.txt", delimiter=",",skiprows=1)
+data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\data_vrije.csv", delimiter=",",skiprows=1)
+data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\data_vrije2.csv", delimiter=",",skiprows=1)
+data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\data_vrije4.csv", delimiter=",",skiprows=1)
 
 # Extract each column into a separate array
 theta_vals, i_P, i_O = data[:, 0], data[:, 1], data[:, 2]
@@ -14,26 +16,30 @@ print("Column 2:", i_P)
 print("Column 3:", i_O)
 
 
-theta_vals=np.array(theta_vals)
+theta_vals=np.array(theta_vals)*np.pi/180
 i_P=np.array(i_P)
-I_P_err=np.ones_like(i_P)
+I_P_err=np.ones_like(i_P)*0.005
 i_O=np.array(i_O)
-I_O_err=np.ones_like(i_O)
+I_O_err=np.ones_like(i_O)*0.005
 I_P=(i_P/(i_P+i_O))
 I_O=(i_O/(i_P+i_O))
 L=1
 lamda=1
 def modelP(params,x):
- phi=params
- return 1-1/2*(1-np.cos(phi))*np.sin(2*x)**2
+ phi,C,delta=params
+ return (1-1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C
 
 def modelO(params,x):
- phi=params
- return 1/2*(1-np.cos(phi))*np.sin(2*x)**2
+ phi,C,delta=params
+ return (1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C
 
 P_data=fp.Data(theta_vals,I_P,I_P_err)
 O_data=fp.Data(theta_vals,I_O,I_O_err)
-print(P_data.fit(model=modelP))
-print(O_data.fit(model=modelO))
-
-
+P_data.show()
+O_data.show()
+print(P_data.fit(model=modelP,initial_guess=(1,0.02,0)))
+print(O_data.fit(model=modelO),initial_guess=(1,0.02,0))
+P_data.fit(model=modelP,initial_guess=(1,0.02,0)).show()
+O_data.fit(model=modelO,initial_guess=(1,0.02,0)).show()
+#safier-1
+#lithium niobaat-2
