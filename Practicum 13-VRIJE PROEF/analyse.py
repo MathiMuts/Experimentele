0988907147951c4f82026d2cc932a8a1c629cce2
Mathias Meersschaut <mathias.meersschaut@icloud.com>
2025-04-25 16:36:22 +0200

    data na pasen

--- Practicum 13-VRIJE PROEF/analyse.py
+++ Practicum 13-VRIJE PROEF/analyse.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 import os
 
 #polarisator moet op 33 deg voor loodrecht
-data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\paas_vrije_proef_2.csv", delimiter=",",skiprows=1)
-# data  = np.loadtxt(r"/home/mathi/Experimentele/Practicum 13-VRIJE PROEF/Vrijdag_data_vrije3.csv", delimiter=",",skiprows=1)
+# data = np.loadtxt(r"C:\Users\micha\OneDrive\Documents\Desktop\Experimentele\Practicum 13-VRIJE PROEF\paas_vrije_proef_2.csv", delimiter=",",skiprows=1)
+data  = np.loadtxt(r"/home/mathi/Experimentele/Practicum 13-VRIJE PROEF/paas_vrije_proef_2.csv", delimiter=",",skiprows=1)
 
 # Extract each column into a separate array
 theta_vals, i_P, i_O = data[:, 0], data[:, 1], data[:, 2]
-print("Column 1:", theta_vals)
-print("Column 2:", i_P)
-print("Column 3:", i_O)
+# print("Column 1:", theta_vals)
+# print("Column 2:", i_P)
+# print("Column 3:", i_O)
 
-ERROR = 0
+ERROR = 0.0001
 theta_vals=np.array(theta_vals)*np.pi/180
 i_P=np.array(i_P)
 I_P_err=np.ones_like(i_P)*ERROR
@@ -26,20 +26,44 @@ I_O=(i_O/(i_P+i_O))
 L=1
 lamda=1
 def modelP(params,x):
- phi,C,delta=params
- return (1-1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C
+ phi,C,delta, d=params
+ return (1-1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C + d
 
 def modelO(params,x):
- phi,C,delta=params
- return (1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C
+ phi,C,delta, d=params
+ return (1/2*(1-np.cos(phi))*np.sin(2*(x+delta))**2)*C + d
+
+def sin(params,x):
+ A, w, phi, c =params
+ return A* np.sin(w*x+phi)+c
 
 P_data=fp.Data(theta_vals,I_P,I_P_err)
 O_data=fp.Data(theta_vals,I_O,I_O_err)
-P_data.show()
-O_data.show()
-print(P_data.fit(model=modelP, initial_guess=(1,0.02,0)).show())
-print(O_data.fit(model=modelO, initial_guess=(1,0.02,0)).show())
-P_data.fit(model=modelP,initial_guess=(1,0.02,0)).show()
-O_data.fit(model=modelO,initial_guess=(1,0.02,0)).show()
+# P_data.show()
+# O_data.show()
+# initial_guess=(0.0002, 1, 0, 0.9993)
+# print(P_data.fit(model=sin, initial_guess=initial_guess).show()) # WERKT!
+
+initial_guess=(0.0002, 1, 0, 0.9993)
+print(P_data.fit(model=modelP, initial_guess=initial_guess).show())
+
+# print(O_data.fit(model=modelO, initial_guess=(1,0.02,0, 1)).show())
+# P_data.fit(model=modelP,initial_guess=(0,0.02,0)).show()
+# O_data.fit(model=modelO,initial_guess=(0,0.02,0)).show()
 #safier-1
 #lithium niobaat-2
+
+
+
+# INFO: Guess for fit
+# model_x = np.linspace(0.9*np.min(P_data.x), 1.1*np.max(P_data.x), 120)
+# # model_y = P_data.model(P_data.minima, model_x)
+# guess_y = sin(initial_guess, model_x)
+
+# fig, ax = plt.subplots(nrows=1, ncols=1, dpi=120, figsize=(5, 3))
+# ax.errorbar(P_data.x, P_data.y, xerr=P_data.dx, yerr=P_data.dy,
+#         marker="o", markersize=1, fmt=" ", color="b", ecolor="black", capsize=2, capthick=0.6, linewidth=0.6)
+# plt.plot(model_x, guess_y, color="r", label="guess")
+# ax.legend()
+# plt.tight_layout()
+# plt.show()
