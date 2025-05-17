a9c815f68c7fd51126f08a27fe0d62c0fc72f775
michailognyanov <michail@ognyanovi.net>
2025-05-17 13:53:55 +0200

    meer icfs en fouten

--- Practicum15_(Project_QSP)/Heatmaps/analyse.py
+++ Practicum15_(Project_QSP)/Heatmaps/analyse.py
@@ -16,9 +16,7 @@ L_RU=np.array([0.6216,-0.8489])
 L_RT=np.array([0.6287,0.6773])
 L_LT=np.array([-0.7923,0.7635])
 
-#error 
-err=0.02
-#np.sqrt((x * dx / r)**2 + (y * dy / r)**2)
+
 
 O_CLU=O_C-O_LU
 O_CRU=O_C-O_RU
@@ -36,33 +34,95 @@ ON_CLU=norm(O_CLU)
 ON_CRU=norm(O_CRU)
 ON_CRT=norm(O_CRT)
 ON_CLT=norm(O_CLT)
-print(ON_CLU)
-print(ON_CRU)
-print(ON_CRT)
-print(ON_CLT)
+# print(ON_CLU)
+# print(ON_CRU)
+# print(ON_CRT)
+# print(ON_CLT)
 
-print(np.std([norm(O_CLU),norm(O_CRU),norm(O_CRT),norm(O_CLT)]))
+# print(np.std([norm(O_CLU),norm(O_CRU),norm(O_CRT),norm(O_CLT)]))
 LN_CLU=norm(L_CLU)
 LN_CRU=norm(L_CRU)
 LN_CRT=norm(L_CRT)
 LN_CLT=norm(L_CLT)
-print(LN_CLU)
-print(LN_CRU)
-print(LN_CRT)
-print(LN_CLT)
+# print(LN_CLU)
+# print(LN_CRU)
+# print(LN_CRT)
+# print(LN_CLT)
 
-print(np.std([norm(L_CLU),norm(L_CRU),norm(L_CRT),norm(L_CLT)]))
+# print(np.std([norm(L_CLU),norm(L_CRU),norm(L_CRT),norm(L_CLT)]))
 
 L=3.5
 
-print(0.44/np.arctan((ON_CLU)/L))
-print(0.44/np.arctan((ON_CRU)/L))
-print(0.44/np.arctan((ON_CLT)/L))
-print(0.44/np.arctan((ON_CRT)/L))
-print(0.44/np.arctan((LN_CLU)/L))
-print(0.44/np.arctan((LN_CRU)/L))
-print(0.44/np.arctan((LN_CLT)/L))
-print(0.44/np.arctan((LN_CRT)/L))
+#
+def icl(th,d,L):
+    return(th/np.arctan(d/L))
+
+print(icl(0.44,ON_CLU,L))
+print(icl(0.44,ON_CRU,L))
+print(icl(0.44,ON_CLT,L))
+print(icl(0.44,ON_CRT,L))
+print(icl(0.44,LN_CLU,L))
+print(icl(0.44,LN_CRU,L))
+print(icl(0.44,LN_CLT,L))
+print(icl(0.44,LN_CRT,L))
+
+#uncertainty
+def err_f(th,d,L):
+    s_d=0.02
+    s_L=0.1
+    return th*np.sqrt(s_d**2*L**2+s_L**2*d**2)*1/(np.arctan(d/L)*np.sqrt(d**2+L**2))
+
+print(err_f(0.44,ON_CLU,L))
+print(err_f(0.44,ON_CRU,L))
+print(err_f(0.44,ON_CLT,L))
+print(err_f(0.44,ON_CRT,L))
+print(err_f(0.44,LN_CLU,L))
+print(err_f(0.44,LN_CRU,L))
+print(err_f(0.44,LN_CLT,L))
+print(err_f(0.44,LN_CRT,L))
+
 
 O_RTLT=O_RT-O_LT
-print(0.61/np.arctan(norm(O_RTLT)/L))
+O_RTLU=O_RT-O_LU
+O_RTRU=O_RT-O_RU
+O_LURU=O_LU-O_RU
+O_LULT=O_LU-O_LT
+O_LTRU=O_LT-O_RU
+
+L_RTLT=L_RT-L_LT
+L_RTLU=L_RT-L_LU
+L_RTRU=L_RT-L_RU
+L_LURU=L_LU-L_RU
+L_LULT=L_LU-L_LT
+L_LTRU=L_LT-L_RU
+
+NO_RTLT=norm(O_RTLT)
+NO_RTLU=norm(O_RTLU)
+NO_RTRU=norm(O_RTRU)
+NO_LURU=norm(O_LURU)
+NO_LULT=norm(O_LULT)
+NO_LTRU=norm(O_LTRU)
+
+NL_RTLT=norm(L_RTLT)
+NL_RTLU=norm(L_RTLU)
+NL_RTRU=norm(L_RTRU)
+NL_LURU=norm(L_LURU)
+NL_LULT=norm(L_LULT)
+NL_LTRU=norm(L_LTRU)
+
+print(icl(0.61,NO_RTLT,L))
+print(icl(0.88,NO_RTLU,L))
+print(icl(0.61,NO_RTRU,L))
+print(icl(0.61,NO_LURU,L))
+print(icl(0.61,NO_LULT,L))
+print(icl(0.88,NO_LTRU,L))
+
+print(icl(0.61,NL_RTLT,L))
+print(icl(0.88,NL_RTLU,L))
+print(icl(0.61,NL_RTRU,L))
+print(icl(0.61,NL_LURU,L))
+print(icl(0.61,NL_LULT,L))
+print(icl(0.88,NL_LTRU,L))
+
+
+# print(0.61/np.arctan(norm(O_RTLT)/L))
